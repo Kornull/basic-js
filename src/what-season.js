7fbd72c51acff6dd5230d23163880ef925323848
Kornull <wolod1987kor@gmail.com>
2022-02-28 22:37:51 +0300

    not run

--- src/what-season.js
+++ src/what-season.js
@@ -1,19 +1,36 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Extract season from given date and expose the enemy scout!
- * 
- * @param {Date | FakeDate} date real or fake date
- * @returns {String} time of the year
- * 
- * @example
- * 
- * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
- */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// /**
+//  * Extract season from given date and expose the enemy scout!
+//  *
+//  * @param {Date | FakeDate} date real or fake date
+//  * @returns {String} time of the year
+//  *
+//  * @example
+//  *
+//  * getSeason(new Date(2020, 02, 31)) => 'spring'
+//  *
+//  */
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+ 
+  let a = date.toString().split(' ')
+
+
+  if (a[1] === 'Oct' || a[1] === 'Nov' || a[1] === 'Sep') {
+    return 'autumn';
+  }
+  if (a[1] === 'Feb' || a[1] === 'Jan' || a[1] === 'Dec') {
+    return 'winter';
+  }
+  if (a[1] === 'Jul' || a[1] === 'Aug' || a[1] === 'Jun') {
+    return 'summer';
+  }
+  if (a[1] === 'Apr' || a[1] === 'Mar' || a[1] === 'May') {
+    return 'spring';
+  }
 }
 
 module.exports = {
