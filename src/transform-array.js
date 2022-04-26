8b12052ce6fe62688ccb8d01ea2334b12f510427
Kornull <wolod1987kor@gmail.com>
2022-04-26 19:25:14 +0300

    transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,22 +1,41 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Create transformed array based on the control sequences that original
- * array contains
- * 
- * @param {Array} arr initial array
- * @returns {Array} transformed array
- * 
- * @example
- * 
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
- * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
- */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function transform(arr) {
+  const textArr = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
+  const newArr = [];
+  if (!Array.isArray(arr)) {
+    throw new NotImplementedError("'arr' parameter must be an instance of the Array! if the arr is not an Array");
+  }
+  if (arr.length == 0) return arr;
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] === '--discard-next' && i !== 0) i++;
+    else if (arr[i] === '--discard-prev' && i !== 0) newArr.pop();
+    else if (arr[i] === '--double-next' && i !== newArr.length - 1) {
+      if (typeof arr[i] === 'number') {
+        newArr.push(arr[i += 1]);
+      }
+    }
+    if (arr[i] === '--double-prev') {
+      if (typeof arr[i] === 'number') {
+        newArr.push(arr[i -= 1]);
+      }
+    }
+    if (!textArr.includes(arr[i]) && arr[i] !== undefined) {
+      newArr.push(arr[i])
+    }
+  }
+
+  return newArr;
 }
+
+// arr.forEach(x => {
+//   if(isNaN(x)) newArr.push(x);
+//   if(newArr.length > 0  && x === '--discard-prev') newArr.pop();
+// })
+// }
 
 module.exports = {
   transform
